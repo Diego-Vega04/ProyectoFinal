a94c3988a7fc3f66730cd157a45761075d62c364
Ares <ares.martin@eviden.com>
2025-04-03 11:23:47 +0200

    Delete angular directory

--- angular/clientes-app/src/app/clientes/clientes.json.ts
+++
@@ -1,9 +0,0 @@
-import { Cliente } from './cliente';
-
-export const CLIENTES: Cliente[] = [
-    {id: 1, nombre: 'Nombre1', apellido: 'Apellido1', createAt: null},
-    {id: 2, nombre: 'Nombre2', apellido: 'Apellido2', createAt: null},
-    {id: 3, nombre: 'Nombre3', apellido: 'Apellido3', createAt: null},
-    {id: 4, nombre: 'Nombre4', apellido: 'Apellido4', createAt: null},
-    {id: 5, nombre: 'Nombre5', apellido: 'Apellido5', createAt: null}
-  ];
