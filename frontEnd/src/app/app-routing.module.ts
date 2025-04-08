328dcab0db0f212547d6bb30f4da7cf89940282c
aresMG <ares.martin@eviden.com>
2025-04-08 09:51:44 +0200

    Componente aislado

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
-import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-  {path: 'user', component: UserComponent, canActivate: [AuthGuard]},
+  {path: 'user', component: UserComponent},
+  //{path: 'user', component: UserComponent, canActivate: [AuthGuard]},
   //{path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
