99e644b995d3b72d71dae0bdf9c39676974bc688
aresMG <ares.martin@eviden.com>
2025-04-08 09:47:41 +0200

    Cambios de color y logo en el form

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -11,7 +10,6 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     LoginComponent
   ],
   imports: [
