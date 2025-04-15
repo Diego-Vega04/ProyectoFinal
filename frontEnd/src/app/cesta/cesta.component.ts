cad39d057c0327420f33879232979f9e9ce2abc3
aresMG <ares.martin@eviden.com>
2025-04-15 13:18:13 +0200

    Commit inicial cesta

---
+++ frontEnd/src/app/cesta/cesta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cesta',
+  standalone: false,
+  templateUrl: './cesta.component.html',
+  styleUrl: './cesta.component.css'
+})
+export class CestaComponent {
+
+}
