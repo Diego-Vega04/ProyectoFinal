edc36ff9b799d7bf4194909a68e0877282c69b08
aresMG <ares.martin@eviden.com>
2025-04-22 09:22:39 +0200

    Cesta version 1

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CestaComponent } from './cesta/cesta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cesta', component: CestaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontEnd/src/app/cesta/cesta.component.ts
+++ frontEnd/src/app/cesta/cesta.component.ts
@@ -1,11 +1,72 @@
 import { Component } from '@angular/core';
-
+interface CestaItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+}
 @Component({
   selector: 'app-cesta',
   standalone: false,
   templateUrl: './cesta.component.html',
   styleUrl: './cesta.component.css'
 })
+
+
 export class CestaComponent {
 
+  cartItems: CestaItem[] = [
+    {
+      id: 1,
+      name: 'Producto 1',
+      price: 29.99,
+      quantity: 2,
+      image: 'https://picsum.photos/200'
+    },
+    {
+      id: 2,
+      name: 'Producto 2',
+      price: 14.5,
+      quantity: 1,
+      image: 'https://picsum.photos/200'
+    }
+  ];
+
+  get resumen(): { envio: number; total: number } {
+    return this.getResumenTotal();
+  }
+
+  // Actualiza la cantidad cuando el input cambia
+  updateQuantity(item: CestaItem) {
+    if (item.quantity < 1) {
+      item.quantity = 1;
+    }
+  }
+
+  // Elimina un producto del carrito
+  removeItem(item: CestaItem) {
+    this.cartItems = this.cartItems.filter(i => i.id !== item.id);
+  }
+
+  // Calcula el subtotal
+  getSubtotal(): number {
+    return this.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  }
+
+  //Total con envío
+  getResumenTotal(): { envio: number; total: number } {
+    const subtotal = this.getSubtotal();
+    const envio = subtotal >= 70 ? 0 : 3.99;
+    let total = subtotal + envio;
+  
+    return { envio, total };
+  }
+
+  // Simula ir al checkout
+  checkout() {
+    alert('Redirigiendo al checkout...');
+    // Aquí podrías usar Router para navegar a otra vista
+    // this.router.navigate(['/checkout']);
+  }
 }
