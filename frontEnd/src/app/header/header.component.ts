c323a966d76fdf7559285f4e90cc48e28ab24951
aresMG <ares.martin@eviden.com>
2025-04-24 10:32:22 +0200

    Barra de búsqueda funcional

--- frontEnd/src/app/header/header.component.ts
+++ frontEnd/src/app/header/header.component.ts
@@ -16,6 +16,10 @@ export class HeaderComponent {
 
   isDarkMode = false;
 
+  nombreBusqueda: string = '';
+  //productos: Producto[] = []; ->descomentar cuando la entidad exista 
+  //productosOG: Producto[] = [];
+
   constructor(public authService: AuthService, private router: Router) { }
 
   // Función para alternar el menú lateral
@@ -32,7 +36,6 @@ export class HeaderComponent {
   closeMenus() {
     this.menuOpen = false;
     this.cartMenuOpen = false;
-
   }
 
   onMiCuentaClick() {
@@ -40,11 +43,12 @@ export class HeaderComponent {
       // Si no está logueado, redirige al login
       this.router.navigate(['/login']);
     } else {
-      // Si está logueado, muestra el menú lateral
-      this.toggleMenu();
+      // Si está logueado, lleva a la página del user
+      this.router.navigate(['/user']);
     }
   }
 
+  //Modo claro oscuro
   toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
     const body = document.body;
@@ -53,6 +57,20 @@ export class HeaderComponent {
       body.setAttribute('data-tema', 'oscuro');
     } else {
       body.removeAttribute('data-tema');
+    }
+  }
+
+  //Función para la barra de búsqueda: (descomentar cuando estén implementadas las entidades y servicios en front)
+  buscarProducto(){
+    const termino = this.nombreBusqueda.toLowerCase().trim();
+
+    if (termino === '') {
+      //this.productos = this.productosOriginales;
+    } else {
+      console.log(termino);
+      //this.productos = this.productosOriginales.filter(producto =>
+      //  producto.nombre.toLowerCase().includes(termino)
+      //);
     }
   }
 }
