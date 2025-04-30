05803cae925f4ce99fe634bdbd100d86d19ee4ef
Ares <ares.martin@eviden.com>
2025-04-30 12:55:26 +0200

    Arreglo error de ficheros

---
+++ backend/src/main/java/com/grupo1/backend/entities/Comentario.java
@@ -0,0 +1,47 @@
+package com.grupo1.backend.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comentarios")
+public class Comentario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nota")
+    private int nota;
+
+    @Column(name = "pros")
+    private String pros;
+
+    @Column(name = "contras")
+    private String contras;
+
+    @Column(name = "opinion")
+    private String opinion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto")
+    private Producto producto;
+}
---
+++ backend/src/main/java/com/grupo1/backend/entities/Producto.java
@@ -0,0 +1,69 @@
+package com.grupo1.backend.entities;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.grupo1.backend.entities.enums.CategoriaProducto;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "marca")
+    private String marca;
+
+    @Column(name = "precio")
+    private BigDecimal precio;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "categoria")
+    @Enumerated(EnumType.STRING)
+    private CategoriaProducto categoria;
+
+    @Column(name = "imagen")
+    private String imagen;
+
+    //varios productos en varios carritos
+    @ManyToMany(mappedBy = "productos")
+    @JsonIgnore
+    private List<Carrito> carritos; 
+
+    //varios productos en varios pedidos
+    @ManyToMany(mappedBy = "productos")
+    @JsonIgnore
+    private List<Pedido> pedidos;
+
+    //un producto en varias reseñas
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Comentario> comentarios;
+    
+}
