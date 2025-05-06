9fefaacc75013e64889e3a524223bf19c000f0a0
aresMG <ares.martin@eviden.com>
2025-05-06 12:23:27 +0200

    Entidad y servicio de carrito

--- frontEnd/src/app/models/carrito.ts
+++ frontEnd/src/app/models/carrito.ts
@@ -1,12 +1,12 @@
 import { User } from "./user";
 
 export class Carrito {
-    id: number;
+    id?: number;
     productos: Producto[];
     user?: User; 
 
     constructor(data?: Partial<Carrito>){
-        this.id = data?.id ?? 0;
+        this.id = data?.id;
         this.productos = data?.productos ?? [];
         this.user = data?.user;
     }
