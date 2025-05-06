e42fa9e4c73ea08b59a6d0abcc80a57660814246
aresMG <ares.martin@eviden.com>
2025-05-06 11:54:08 +0200

    Entidad y servicio de usuario

---
+++ frontEnd/src/app/models/user.ts
@@ -0,0 +1,27 @@
+export class User{
+    id: number;
+    nombre: string;
+    apellidos: string;
+    email: string;
+    password: string;
+    cp: number;
+    direccion: string;
+    rol: 'ADMIN' | 'USER';
+    carrito?: Carrito;
+    favoritos?: Favoritos;
+    pedidos?: Pedido[];
+
+    constructor(data?: Partial<User>){
+        this.id = data?.id ?? 0;
+        this.nombre = data?.nombre ?? '';
+        this.apellidos = data?.apellidos ?? '';
+        this.email = data?.email ?? '';
+        this.password = data?.password ?? '';
+        this.cp = data?.cp ?? 0;
+        this.direccion = data?.direccion ?? '';
+        this.rol = data?.rol ?? 'USER';
+        this.carrito = data?.carrito;
+        this.favoritos = data?.favoritos;
+        this.pedidos = data?.pedidos ?? [];
+    }
+}
