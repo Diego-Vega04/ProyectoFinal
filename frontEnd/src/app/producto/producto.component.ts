bc5e1369503cdbf4aa0de8529734e878a6d98627
aresMG <ares.martin@eviden.com>
2025-05-09 10:49:14 +0200

    Componente producto vinculado

--- frontEnd/src/app/producto/producto.component.ts
+++ frontEnd/src/app/producto/producto.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ReviewDialogComponent } from './review-dialog/review-dialog.component';
+import { Producto } from '../models/producto';
+import { ActivatedRoute } from '@angular/router';
+import { ProductoService } from '../services/producto.service';
 
 @Component({
   selector: 'app-producto',
@@ -8,8 +11,25 @@ import { ReviewDialogComponent } from './review-dialog/review-dialog.component';
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.css'
 })
-export class ProductoComponent {
-  constructor(public dialog: MatDialog) {}
+export class ProductoComponent implements OnInit{
+  productId!: number;
+  product!: Producto | undefined;
+
+  constructor(public dialog: MatDialog, private route: ActivatedRoute, private productService: ProductoService) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.productId = Number(params.get('id'));
+      this.productService.getById(this.productId).subscribe({
+        next: (data) => {
+          this.product = data;
+        },
+        error: (err) => {
+          console.error('Error al cargar el producto', err);
+        }
+      });
+    });
+  }
 
   openReviewDialog(): void {
     const dialogRef = this.dialog.open(ReviewDialogComponent, {
