cf62a93ebd4166bae5cd696b70c488e4499b19ec
aresMG <ares.martin@eviden.com>
2025-05-20 13:17:45 +0200

    Gestión de productos sin servicios

--- frontEnd/src/app/vista-admin/vista-admin.component.ts
+++ frontEnd/src/app/vista-admin/vista-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-vista-admin',
@@ -8,11 +9,8 @@ import { Component } from '@angular/core';
 })
 export class VistaAdminComponent {
 
-  gestionProduct: boolean = true
-
-  gestionProductos() {
-     this.gestionProduct = !this.gestionProduct;
-  }
+  gestionProduct: boolean = true;
+  editProduct = new Map<number, boolean>();
 
   productos = [
     { id: 1, nombre: 'Camiseta', precio: 19.99, stock: 20 },
@@ -20,4 +18,33 @@ export class VistaAdminComponent {
     { id: 3, nombre: 'Zapatillas', precio: 59.99, stock: 10 },
     { id: 4, nombre: 'Chaqueta', precio: 79.99, stock: 5 },
   ];
+
+  gestionProductos() {
+    this.gestionProduct = !this.gestionProduct;
+  }
+
+  editarProducto(id: number) {
+    const actual = this.editProduct.get(id) || false;
+    this.editProduct.set(id, !actual);
+  }
+
+  eliminarProducto() {
+    Swal.fire({
+      title: "¿Eliminar este producto de la base de datos?",
+      text: "Esta acción no se podrá reveritr",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, borrar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Producto eliminado",
+          text: "El producto ha sido eliminado correctamente.",
+          icon: "success"
+        });
+      }
+    });
+  }
 }
